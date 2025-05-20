9f784388cdce75e053ba0985bf7adc76ca34783a
fabianRivero <fabianarj467@gmail.com>
2025-05-20 13:58:01 -0400

    ajustes para correr en netlify

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   },
-   base: '/',
   server: {
     open: true
   }
